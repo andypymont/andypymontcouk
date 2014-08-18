0ef6b1c9dd9251e569de1cc14e2946b05d5714ce
Andy Pymont <andypymont@gmail.com>
2014-08-18 14:01:11 +0100

    Added missing render_template import

--- andypymont.py
+++ andypymont.py
@@ -3,9 +3,14 @@ from werkzeug.wsgi import DispatcherMiddleware
 from app import app as app_homepage
 import fantasydrafts
 
+from flask import render_template
+
 app = DispatcherMiddleware(app_homepage, {'/drafts': fantasydrafts.app})
 
 @app_homepage.route('/')
 def index():
 	latest_drafts = fantasydrafts.db.get_latest_drafts()
 	return render_template('index.html', drafts=latest_drafts)
+
+if __name__ == '__main__':
+	app.run()
