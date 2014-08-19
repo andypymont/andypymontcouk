1d74f619cd65a72fda71e784c1c5c6df95c6b0e0
Andy Pymont <andypymont@gmail.com>
2014-08-19 16:25:28 +0100

    Added fantasypl application to the middleware

--- andypymont.py
+++ andypymont.py
@@ -2,14 +2,17 @@ from werkzeug.wsgi import DispatcherMiddleware
 
 from app import app as app_homepage
 import fantasydrafts
+import fantasypl
 
 from flask import render_template
 from flask_sslify import SSLify
 
 sslify_fantasydrafts = SSLify(fantasydrafts.app)
+sslify_fantasypl = SSLify(fantasypl.app)
 sslify_homepage = SSLify(app_homepage)
 
-app = DispatcherMiddleware(app_homepage, {'/drafts': fantasydrafts.app})
+app = DispatcherMiddleware(app_homepage, {'/drafts': fantasydrafts.app,
+										  '/fantasypl': fantasypl.app})
 
 @app_homepage.route('/')
 def index():
